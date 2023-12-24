22b60856f6eee2a7453fe0233c3a8e5677ae02c1
Alexandre Flion <huntears@kreog.com>
2023-12-24 19:24:36 +0100

    Add data overwrites in insert function

--- src/hashmap/hm_insert.c
+++ src/hashmap/hm_insert.c
@@ -10,20 +10,47 @@
 
 #include "hashmap.h"
 
-// TODO (huntears): Handle data overwrites
-int hm_insert(hashmap_t *hm, const char *key, const char *value)
+// hashed_key has already been computed, no need to recompute
+static int insert_new_node(
+    bucket_t *bck, const char *key, const char *value, uint64_t hashed_key
+)
 {
-    uint64_t hashed_key = hm->hash_function(key, (uint32_t) strlen(key));
     bucket_node_t *new_node = malloc(sizeof(bucket_node_t));
-    uint64_t bucket_index = hashed_key % hm->num_buckets;
 
     if (!new_node)
         return 1;
     new_node->hashed_key = hashed_key;
     new_node->key = strdup(key);
+    if (!new_node->key) {
+        free(new_node);
+        return 1;
+    }
     new_node->data = strdup(value);
-    new_node->next = hm->buckets[bucket_index].nodes;
-    hm->buckets[bucket_index].num_nodes++;
-    hm->buckets[bucket_index].nodes = new_node;
+    if (!new_node->data) {
+        free(new_node->key);
+        free(new_node);
+        return 1;
+    }
+    new_node->next = bck->nodes;
+    bck->num_nodes++;
+    bck->nodes = new_node;
     return 0;
+}
+
+static int update_node(bucket_node_t *node, const char *value)
+{
+    free(node->data);
+    node->data = strdup(value);
+    return !node->data;
+}
+
+int hm_insert(hashmap_t *hm, const char *key, const char *value)
+{
+    uint64_t hashed_key = hm->hash_function(key, (uint32_t) strlen(key));
+    uint64_t bucket_index = hashed_key % hm->num_buckets;
+
+    for (bucket_node_t *n = hm->buckets[bucket_index].nodes; n; n = n->next)
+        if (n->hashed_key == hashed_key && !strcmp(n->key, key))
+            return update_node(n, value);
+    return insert_new_node(&hm->buckets[bucket_index], key, value, hashed_key);
 }
