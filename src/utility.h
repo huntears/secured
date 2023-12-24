4ef40c8c2582f4e70bdd6a1a865eac2dc09bf68e
Alexandre Flion <huntears@kreog.com>
2023-12-24 17:20:24 +0100

    Add UNUSED macro

---
+++ src/utility.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** secured
+** File description:
+** utility
+*/
+
+#ifndef BBF4072A_9FEE_4A38_B328_D8142D4D9066
+#define BBF4072A_9FEE_4A38_B328_D8142D4D9066
+
+#define UNUSED __attribute__((unused))
+
+#endif /* BBF4072A_9FEE_4A38_B328_D8142D4D9066 */
